d1657696a5dfa568a15fc2255273ad8a30ffb27c
quattrokki <160909419+quattrokki@users.noreply.github.com>
2024-03-16 19:08:16 +0600

    Add files via upload

---
+++ 18.03.24/main.js
@@ -0,0 +1,43 @@
+// Напишите функцию, которая принимает в себя строку(параметр) и убирает из неё все пробелы. (возвращает строку без пробелов)
+//"    hello my friends   " --> "hellomyfriends"
+
+
+//  let probel = "    hello my friends   "
+// function removeSpaces(str) {
+// return str.replace(/\s/g, '');
+// }
+// console.log(removeSpaces("    hello my friends   ")); 
+
+
+// Напишите функцию, которая принимает массив и возвращает его последний элемент
+
+// const arr = [1,2,3,4,5,6];
+// function getLastElement(arr) {
+//     return arr[arr.length -1];
+// }
+// console.log(getLastElement(arr));
+
+//Напишите функцию, которая принимает два массива, соединяет их и возвращает
+
+// function combineArrays(arr1, arr2) {
+//     return arr1.concat(arr2);
+// }
+
+// const numbers = [1,2,3,4,5];
+// const strings = ['a','b','c','d'];
+
+// const combinedArray = combineArrays(numbers, strings);
+// console.log(combinedArray);
+
+//Напишите функцию, которая принимает в себя строку и делает первую букву заглавной
+// Например: 'привет' --> 'Привет'
+
+
+// function firstLetter(str) {
+//     return str.charAt(0).toUpperCase() + str.slice(1);
+//   }
+//   console.log(firstLetter('привет'));
+
+
+
+
